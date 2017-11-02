955381f94d2195ff3af374f8f422b35c3b6bb649
Derek Lucas <d@derekplucas.com>
2017-11-02 11:03:43 -0400

    Joystick/player controls

---
+++ Assets/Scripts/CreatePlayers.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatePlayers : MonoBehaviour {
+	public GameObject player;
+	private List<GameObject> players = new List<GameObject>();
+
+  private int joystickId;
+  private string joystickName;
+
+	void Update () {
+		// Look for new joysticks
+
+    var key = CheckForKey();
+    if (key != KeyCode.None) {
+			if (key == KeyCode.W) {
+				createPlayer("Keyboard 1");
+			} else if (key == KeyCode.UpArrow) {
+				createPlayer("Keyboard 2");
+			} else {
+	      // Convert Joystick3Button42 to 3
+	      try {
+	        joystickId = int.Parse(key.ToString().Replace("Joystick", "").Split(new string[] { "Button" }, System.StringSplitOptions.RemoveEmptyEntries)[0]);
+	        joystickName = Input.GetJoystickNames()[joystickId - 1];
+
+					createPlayer("Joystick " + joystickId);
+
+	        Debug.Log("#" + joystickId + " - " + joystickName);
+	      } catch (System.Exception) { }
+			}
+    }
+	}
+
+	private void createPlayer(string id) {
+		if (players.Exists(x => x.name == id)) {
+			return;
+		}
+
+		Debug.Log("New Player:" + id);
+		GameObject newPlayer = Instantiate(player);
+		newPlayer.name = id;
+
+    PlayerControl control = newPlayer.GetComponent<PlayerControl>();
+		control.controller = id;
+
+		players.Add(newPlayer);
+	}
+
+  private KeyCode CheckForKey(string id = "") {
+    foreach (KeyCode k in System.Enum.GetValues(typeof(KeyCode))) {
+
+      // Get "JoystickNButtonX" keys but not "JoystickButtonX" keys
+      if ((k.ToString().Contains("Joystick" + id) && !k.ToString().Contains("JoystickButton")) ||
+					k.ToString() == "W" || k.ToString() == "UpArrow") {
+        if (Input.GetKeyDown(k)) {
+          return k;
+        }
+      }
+    }
+    return KeyCode.None;
+  }
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+	public GameObject bullet;
+
+	public string controller = "Keyboard 1";
+	public string horizontal = "Horizontal";
+	public string vertical = "Vertical";
+	public float speed = 1.0f;
+	private Vector3 fireDirection;
+
+	public float shootDelay = 0.1f;
+
+	private bool canShoot = true;
+
+
+	void ResetShot () {
+		canShoot = true;
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 direction = Vector3.right * Input.GetAxis(horizontalAxis()) + Vector3.up * Input.GetAxis(verticalAxis());
+
+		if (direction.sqrMagnitude > 0.0f) {
+			transform.position += direction.normalized * speed * Time.deltaTime;
+
+			if (direction.x != 0) {
+				SpriteRenderer sprite = gameObject.GetComponent<SpriteRenderer>();
+				sprite.flipX = direction.x < 0;
+
+				fireDirection.x = direction.x;
+			}
+
+			if (direction.y != 0) {
+				fireDirection.y = direction.y;
+			}
+		}
+
+		if(canShoot) {
+			Instantiate(bullet, transform.position, Quaternion.LookRotation(fireDirection, Vector3.up));
+
+			canShoot = false;
+			Invoke("ResetShot", shootDelay);
+		}
+	}
+
+	private string horizontalAxis() {
+		return controller + " " + horizontal;
+	}
+
+	private string verticalAxis() {
+		return controller + " " + vertical;
+	}
+}
